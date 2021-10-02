b6372666d16c51bdfe382f8b1125f3ef234b29c8
georges <gf.nodari@hotmail.com>
2021-10-02 18:37:14 +0200

    project init

---
+++ public/js/main.js
@@ -0,0 +1,63 @@
+// let number = prompt("entrez un nombre")
+
+// switch (number%2==0) {
+//     case true:
+//         console.log("chiffre paire");
+//         break;
+//     case false:
+//         console.log("chiffre impaire");
+//         break;
+
+//     default:
+//         break;
+// }
+
+// exo2
+
+let number = parseInt(prompt("entre un nombre de 1 a 12"))
+
+switch (number) {
+    case 1:
+        console.log(nu);
+        break;
+    case 2:
+        console.log("mois de fevrier");
+        break;
+    case 3:
+        console.log("mois de mars");
+        break;
+    case 4:
+        console.log("mois de avril");
+        break;
+    case 5:
+        console.log("mois de avril");
+        break;
+    case 6:
+        console.log("mois de mai");
+        break;
+    case 7:
+        console.log("mois de juin");
+        break;
+    case 8:
+        console.log("mois de juillet");
+        break;
+    case 9:
+        console.log("mois de aout");
+        break;
+    case 10:
+        console.log("mois de septembre");
+        break;
+    case 11:
+        console.log("mois de octobre");
+        break;
+    case 12:
+        console.log("mois de novembre");
+        break;
+    case 1:
+        console.log("mois de decembre");
+        break;
+
+    default:
+        console.log("ERREUR : entre un chiffre de 1 a 12 seulement!");
+        break;
+}
